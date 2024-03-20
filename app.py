9b1e850830077948113620adfa0ceb3904e10a89
Olowu Olumide <71939057+axtonleon@users.noreply.github.com>
2024-03-20 13:20:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 # Initialize your chatbot components here
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 
 # Custom CSS for styling
 st.markdown("""
